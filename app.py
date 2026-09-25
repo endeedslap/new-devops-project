6c2938faa90e3f575bece2ae0f042d5b2b541633
isse ali <lifelongcot652@gmail.com>
2026-09-25 07:54:59 -0400

    fix: correct DevOps typo in index response

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ metrics.info("app_info", "Application information", version=os.getenv("APP_VERSI
 def index():
     logger.info("Root endpoint requested")
     return jsonify({
-        "message": "Devps Cloud Project is running",
+        "message": "Devops Cloud Project is running",
         "version": os.getenv("APP_VERSION", "1.0.0")
     })
 
